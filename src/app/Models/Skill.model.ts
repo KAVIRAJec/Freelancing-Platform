7d88e7ab2d5aa6123a589b207afd4a37e29b1fed
KAVIRAJec <kavirajppm2003@gmail.com>
2025-07-09 11:36:46 +0530

    Added frontend & backend[ADD]

--- src/app/Models/Skill.model.ts
+++
@@ -1,9 +0,0 @@
-export class SkillModel {
-    public skillId: string;
-    public name: string;
-
-    constructor() {
-        this.skillId = '';
-        this.name = '';
-    }
-}
--- src/app/NgRx/Client/client.selector.ts
+++
@@ -1,9 +0,0 @@
-import { createFeatureSelector, createSelector } from "@ngrx/store";
-import { ClientState } from "./clientState";
-
-export const selectClientState = createFeatureSelector<ClientState>('client');
-
-export const selectAllClients = createSelector(selectClientState, state => state.clients);
-export const selectClientPagination = createSelector(selectClientState, state => state.pagination);
-export const selectClientLoading = createSelector(selectClientState, state => state.loading);
-export const selectClientError = createSelector(selectClientState, state => state.error);
